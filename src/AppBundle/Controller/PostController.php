c80976fcb2866456b6c07f9c843ddbe930b4163d
--quiet <ckim4@ip-172-31-19-160.us-west-1.compute.internal>
2017-04-05 21:13:23 +0000

    Old version of postcontroller

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -2,8 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Users;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,36 +15,9 @@ class PostController extends Controller
      */
     public function indexAction(Request $request)
     {
-        /*
         // replace this example code with whatever you need
         return $this->render('gatortraders/postview.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
-        */
-
-
-        $username = $_GET["Studentemail"];
-        $password = $_GET["Password"];
-
-        $userdets = $this->getDoctrine()
-            ->getRepository('AppBundle:Users')
-            ->findAll();
-
-
-        foreach($userdets as $e)
-        {
-            if ($e->getStudentemail() == $username && $e->getPassword() == $password )
-               // if (1==2)
-                {
-
-                return $this->render('gatortraders/welcome.html.twig', [
-                    'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-                ]);
-
-                }
-        }
-
-
-        return $this->render('gatortraders/postview.html.twig', array('viewUserDets' => $userdets));
     }
 }
