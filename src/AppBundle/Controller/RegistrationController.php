ca86d43ea9b160510eacee3988658c7aa5a6215e
--quiet <kkwok@ip-172-31-19-160.us-west-1.compute.internal>
2017-04-07 20:36:32 +0000

    Merge conflict

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -15,26 +15,32 @@ class RegistrationController extends Controller
      */
     public function indexAction(Request $request)
     {
-            //$username = $_GET["email"];
+        $userdets = $this->getDoctrine()
+            ->getRepository('AppBundle:Users')
+            ->findAll();
+
+            $username = $_GET["email"];
             $first_name = $_GET["first_name"];
             $last_name = $_GET["last_name"];
             $password = $_GET["password"];
 
+                print "Hello";
 
             $users = new Users();
 
+            $users->setStudentemail($username);
             $users->setFirstName($first_name);
             $users->setLastName($last_name);
             $users->setPassword($password);
-
+            $users->setRating(5);
+            $users->setVerification("1");
 
             $em = $this->getDoctrine()
                 //     ->getRepository('AppBundle:Users')
                 ->getManager();
 
             $em->persist($users);
-            $em->flush();
-
+            //$em->flush();
 
         // replace this example code with whatever you need
         return $this->render('gatortraders/registration.html.twig', [
