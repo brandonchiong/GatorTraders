d29fe4d9d132548455740eea7d8c4a06f2e9c47f
Marc Panlilio <mpanlili@ip-172-31-19-160.us-west-1.compute.internal>
2017-04-17 02:13:07 +0000

    Added files needed to link the pages

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Users;
+
 
 class RegistrationController extends Controller
 {
@@ -13,9 +16,44 @@ class RegistrationController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('gatortraders/registration.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+
+        $user = new Users();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setVerification(false);
+           /* $userEmail = $user->getStudentemail();
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+
+            if($em->getRepository('AppBundle\Entity\Users')->findOneBy(array('studentemail' => $userEmail)) != null) {
+                print("The email existed already");
+
+                return $this->render(
+                    'gatortraders/registration.html.twig'
+                    ,array('form' => $form->createView())
+                );
+            };
+
+
+
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+            print($user->getStudentemail());
+*/
+            return $this->redirectToRoute('welcome');
+        }
+
+        return $this->
+        render('gatortraders/registration.html.twig'
+        ,array('form' => $form->createView())
+        );
     }
+
 }
