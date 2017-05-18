8ea550c52e890c276d5a8d778f9e4dc9991a4419
kendrickkwok <kendrick.j.kwok@gmail.com>
2017-05-18 01:37:22 +0000

    this is a test

--- src/AppBundle/Controller/ResultController.php
+++ src/AppBundle/Controller/ResultController.php
@@ -15,30 +15,13 @@ class ResultController extends Controller
      */
     public function showAction()
     {
-        $postId = $_GET["postId"];
+
         $itemid = $_GET["name"];
 
         $userdets = $this->getDoctrine()
             ->getRepository('AppBundle:Table1')
             ->findAll();
 
-        $userdets1 = $this->getDoctrine()
-            ->getRepository('AppBundle:Post')
-            ->findAll();
-
-        foreach ($userdets1 as $post) {
-
-            if ($postId == $post->getPostid()) {
-                $post->setFlag(1);
-                print $post->getFlag();
-                print $post->getPosttitle();
-            }
-        }
-
-
-        $em2 = $this->getDoctrine()->getManager();
-        //$em2->persist($userdets1);
-        $em2->flush();
         return $this->render('gatortraders/result.html.twig', array('viewUserDets' => $userdets, 'searchkey' => $itemid));
 
     }
